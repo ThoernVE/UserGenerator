f3dde6d1cfef9d7288c374eef513f59a075b9075
Viktor Thörn <Viktor.e.thorn@hotmail.com>
2024-11-25 17:43:04 +0100

    added a function to save data into MyDocuments as a json-file. Also added a switch to make reader able to choose whether data should be printed or saved. Put in error handling for the options and made the switch function with both numpad and regular numbers.

--- Program.cs
+++ Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace UserGenerator
 {
@@ -15,15 +17,71 @@ namespace UserGenerator
 
         public static void Run()
         {
-            Console.WriteLine("Hello and welcome to the UserGenerator!/n" +
+            Console.WriteLine("Hello and welcome to the UserGenerator! \n" +
                 "How many Users would you like to generate?");
-            int numberOfUsers = Convert.ToInt32(Console.ReadLine());
-            RandomGenerator.GenerateUsers(numberOfUsers);
+            try
+            {
+                int numberOfUsers = Convert.ToInt32(Console.ReadLine());
+                RandomGenerator.GenerateUsers(numberOfUsers);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input, please try again");
+                Console.Clear();
+                Run();
+            }
+            Console.WriteLine("What would you like to do with the data? Please type the correspondning number to choose\n" +
+                "1. Print\n" +
+                "2. Convert to JSON");
+            optionSwitch();
+        }
 
+        private static void optionSwitch()
+        {
+
+            switch (Console.ReadKey().Key)
+            {
+
+                case ConsoleKey.NumPad1:
+                case ConsoleKey.D1:
+                    Print();
+                    break;
+                case ConsoleKey.NumPad2:
+                case ConsoleKey.D2:
+                    ConvertJSON();
+                    break;
+                default:
+                    Console.WriteLine("\nThat input was not possible. Press any button to try again");
+                    Console.ReadKey();
+                    Console.Clear();
+                    Run();
+                    break;
+            }
+
+
+        }
+
+        public static void Print()
+        {
             foreach (var user in User.users)
             {
                 Console.WriteLine(user);
             }
+        }
+
+        private static void ConvertJSON()
+        {
+            var serializedObject = Newtonsoft.Json.JsonConvert.SerializeObject(User.users);
+
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var filepath = Path.Combine(path, "UserData.json");
+
+            using (StreamWriter sw = new StreamWriter(filepath))
+            {
+                sw.Write(serializedObject);
+            }
+
+            Console.WriteLine("\nYour data has been placed in: 'MyDocuments', under the filename 'UserData.json'");
         }
     }
 }
