8a19ec5e74d3f4e2a301af6fec10ca582d64e9cf
ThoernVE <viktor.e.thorn@hotmail.com>
2024-11-26 09:18:34 +0100

    added if/else statements to make sure to account for leap days aswell as different amount of days in a month

--- RandomGenerator.cs
+++ RandomGenerator.cs
@@ -18,10 +18,27 @@ namespace UserGenerator
                 string lastName = User.lastNames[random.Next(User.lastNames.Count)];
                 int birthYear = random.Next(1924, 2024);
                 int birthMonth = random.Next(1, 12);
-                int birthDay = random.Next(1, 31);
+                int birthDay = 0;
                 string address = User.addresses[random.Next(User.addresses.Count)];
                 int addressNumber = random.Next(1, 100);
                 int postalCode = User.postalCodes[random.Next(User.postalCodes.Count)];
+
+                if (birthYear % 4 == 0 && birthMonth == 2)
+                {
+                    birthDay = random.Next(1, 29);
+                }
+                else if (birthMonth == 2)
+                {
+                    birthDay = random.Next(1, 28);
+                }
+                else if (birthMonth == 1 || birthMonth == 3 || birthMonth == 5 || birthMonth == 7 || birthMonth == 8 || birthMonth == 10 || birthMonth == 12)
+                {
+                    birthDay = random.Next(1, 31);
+                }
+                else
+                {
+                    birthDay = random.Next(1, 30);
+                }
 
                 User.users.Add(new User(firstName, lastName, birthYear, birthMonth, birthDay, address, addressNumber, postalCode));
 
