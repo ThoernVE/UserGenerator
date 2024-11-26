023840800846641b6e3f3f5065cecf2f15de8ae3
ThoernVE <viktor.e.thorn@hotmail.com>
2024-11-26 09:30:21 +0100

    moved if/else statement to separate function to clean up the code.

--- RandomGenerator.cs
+++ RandomGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,32 +19,34 @@ namespace UserGenerator
                 string lastName = User.lastNames[random.Next(User.lastNames.Count)];
                 int birthYear = random.Next(1924, 2024);
                 int birthMonth = random.Next(1, 12);
-                int birthDay = 0;
+                int birthDay = DaysPerMonth(birthYear, birthMonth);
                 string address = User.addresses[random.Next(User.addresses.Count)];
                 int addressNumber = random.Next(1, 100);
                 int postalCode = User.postalCodes[random.Next(User.postalCodes.Count)];
 
-                if (birthYear % 4 == 0 && birthMonth == 2)
-                {
-                    birthDay = random.Next(1, 29);
-                }
-                else if (birthMonth == 2)
-                {
-                    birthDay = random.Next(1, 28);
-                }
-                else if (birthMonth == 1 || birthMonth == 3 || birthMonth == 5 || birthMonth == 7 || birthMonth == 8 || birthMonth == 10 || birthMonth == 12)
-                {
-                    birthDay = random.Next(1, 31);
-                }
-                else
-                {
-                    birthDay = random.Next(1, 30);
-                }
-
                 User.users.Add(new User(firstName, lastName, birthYear, birthMonth, birthDay, address, addressNumber, postalCode));
 
-                
+                static int DaysPerMonth(int birthYear, int birthMonth)
+                {
+                    if (birthYear % 4 == 0 && birthMonth == 2) //if/else statements to account for different amount of days per months aswell as leap year.
+                    {
+                        return random.Next(1, 29);
+                    }
+                    else if (birthMonth == 2)
+                    {
+                        return  random.Next(1, 28);
+                    }
+                    else if (birthMonth == 1 || birthMonth == 3 || birthMonth == 5 || birthMonth == 7 || birthMonth == 8 || birthMonth == 10 || birthMonth == 12)
+                    {
+                        return  random.Next(1, 31);
+                    }
+                    else
+                    {
+                        return  random.Next(1, 30);
+                    }
+                }
             }
+
         }
     }
 }
